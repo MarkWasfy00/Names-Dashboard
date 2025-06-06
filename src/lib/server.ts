d6129bc9b99c69dd8a4c84bd2fc2dcddf78c3c18
kali <mizzoplays@gmail.com>
2025-06-06 05:20:36 +0300

    added backend url https

--- src/lib/server.ts
+++ src/lib/server.ts
@@ -1,4 +1,4 @@
 export const server = {
-    baseUrl: "http://34.171.4.42:5050/api",
-    websocketUrl: "http://34.171.4.42:5050/youtubechathub",
+    baseUrl: "https://youtubeai.it.com/api",
+    websocketUrl: "https://youtubeai.it.com/youtubechathub",
 }
